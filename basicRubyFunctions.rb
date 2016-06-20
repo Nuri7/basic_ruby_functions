27d15624b15af7d5cf1a1ba0d314a923a3dc44da
Nuri Bayram <nuri.bayram@me.com>
2016-06-20 14:33:00 -0400

    initial commit

---
+++ basicRubyFunctions.rb
@@ -0,0 +1,15 @@
+# Create a function that takes a string and adds the phrase "Only in America!" to the end of it
+def add_america_phrase(phrase)
+	puts phrase + " Only in America!"
+end
+
+# call to see if function works
+add_america_phrase("Are people crazy?");
+
+# Create a function to find the maximum value in an array of numbers. For instance: [100,10,-1000]
+# should return 100. Do not use Ruby's built-in .max function.
+
+# put array with numbers into memory
+array_with_numbers = [1, 3, 5, 2]
+
+
